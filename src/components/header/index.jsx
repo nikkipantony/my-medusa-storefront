a7c97bec12581cfa5f5f6ec53c5a16c449303077
Nikki Pantony <nikkipantony@gmail.com>
2022-12-30 15:51:21 +0000

    Update index.jsx

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,86 +1,12 @@
 import { Link } from "gatsby"
 import React, { useState } from "react"
 import Logo from "../../icons/logo.svg"
-import AccountPopover from "./account-popover"
+// import AccountPopover from "./account-popover"
 import Banner from "./banner"
 import CartPopover from "./cart-popover"
 import HeaderLink from "./header-link"
 import MobileMenu from "./mobile-menu"
 import RegionPopover from "./region-popover"
-
-const mockData = {
-  customer: {
-    first_name: "Kasper",
-    last_name: "F. Kristensen",
-  },
-  cart: {
-    currency_code: "DKK",
-    items: [
-      {
-        title: "Medusa Tote",
-        amount: 12500,
-        quantity: 1,
-        thumbnail:
-          "https://medusa-public-images.s3.eu-west-1.amazonaws.com/tshirt.png",
-      },
-      {
-        title: "Medusa Cover",
-        amount: 9000,
-        quantity: 1,
-        thumbnail:
-          "https://medusa-public-images.s3.eu-west-1.amazonaws.com/tshirt.png",
-      },
-      {
-        title: "Medusa Sweatshirt",
-        amount: 28000,
-        quantity: 2,
-        thumbnail:
-          "https://medusa-public-images.s3.eu-west-1.amazonaws.com/tshirt.png",
-      },
-    ],
-  },
-  regions: [
-    {
-      id: "1",
-      name: "Denmark",
-      currency_code: "DKK",
-      countries: [
-        {
-          display_name: "Denmark",
-        },
-      ],
-    },
-    {
-      id: "2",
-      name: "Norway",
-      currency_code: "NOK",
-      countries: [
-        {
-          display_name: "Norway",
-        },
-      ],
-    },
-    {
-      id: "3",
-      name: "Europe",
-      currency_code: "EUR",
-      countries: [
-        {
-          display_name: "Germany",
-        },
-        {
-          display_name: "France",
-        },
-        {
-          display_name: "Italy",
-        },
-        {
-          display_name: "Spain",
-        },
-      ],
-    },
-  ],
-}
 
 const Header = () => {
   const [open, setOpen] = useState(false)
@@ -127,10 +53,10 @@ const Header = () => {
 
           <div className="flex items-center justify-end">
             <div className="hidden lg:flex">
-              <RegionPopover regions={mockData.regions} />
-              <AccountPopover customer={mockData.customer} />
+              <RegionPopover />
+              {/* <AccountPopover /> */}
             </div>
-            <CartPopover cart={mockData.cart} />
+            <CartPopover />
           </div>
         </nav>
       </header>
